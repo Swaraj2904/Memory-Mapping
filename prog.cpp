d2b62f49df9e1a62a5dc3c15743c7be2bb882458
Swaraj2904 <swarajpaul29@gmail.com>
2015-02-01 22:52:38 +0530

    Create prog.cpp

---
+++ prog.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+#include <math>
+
+using namespace std;
+/* Function to generate random numbers and store them in a file */
+void generate()
+{
+  for(int i = 0; i <= 100000; i++)
+  {
+    
+  }
+}
+public void main()
+{
+  
+}
